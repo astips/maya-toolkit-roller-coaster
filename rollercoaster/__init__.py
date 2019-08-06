dfc4c75840e24504973ed0f427338cc0d4df0730
astips <thebeatles2006@163.com>
2019-08-06 17:28:43 +0800

    change dialog type

--- rollercoaster/__init__.py
+++ rollercoaster/__init__.py
@@ -4,7 +4,7 @@ import os
 
 
 __name__ = 'RollerCoaster'
-__version__ = '1.1.0'
+__version__ = '1.1.2'
 __author__ = 'astips'
 __package__ = 'rollercoaster'
 __build__ = 'Cython 0.29, build on December 13, 2018'
